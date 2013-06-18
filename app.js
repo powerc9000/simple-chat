aeb48c37fc2e3ee58e14cf2370464395a7588414
powerc9000 <clay.murray8@gmail.com>
2013-06-18 11:36:57 -0600

    added in a chat list

--- app.js
+++ app.js
@@ -19,12 +19,13 @@ app.configure(function(){
   app.use(flash());
 });
 //test
-server.listen(80);
+server.listen(3000);
 app.get("/", function(req, res){
 	res.render("index.html");
 });
-
+var online = [];
 socket.sockets.on("connection", function(s){
+
 	s.on("message", function(message){
 		s.get("username", function(err, username){
 			socket.sockets.emit("message", username, message);
@@ -32,9 +33,22 @@ socket.sockets.on("connection", function(s){
 		
 	});
 	s.on("username", function(username){
+		var idx = online.push(username) -1;
 		s.set("username", username);
 		socket.sockets.emit("message", username + " joined the room!");
-	})
+		s.set("index", idx);
+		socket.sockets.emit("online", online);
+	});
+	s.on("disconnect", function(){
+		s.get("index", function(err, index){
+			s.get("username", function(err, username){
+				online.splice(index, 1);
+				socket.sockets.emit("online", online);
+				socket.sockets.emit("message", username + " left the room!");
+			})
+		})
+		
+	});
 });
 
 
