64720db5f4c68117ebb768bd16b03c67587486a9
powerc9000 <clay.murray8@gmail.com>
2013-06-20 11:47:11 -0600

    now remebers messages with redis!

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require("express"),
 	app = express(),
 	server = require('http').createServer(app),
 	socket = require("socket.io").listen(server),
-	flash = require('connect-flash');
+	flash = require('connect-flash')
+	RedisStore = require("connect-redis")(express),
+	redis = require("redis"),
+	client = redis.createClient();
 
 
 app.configure(function(){
@@ -13,9 +16,8 @@ app.configure(function(){
 	app.use(express.static(__dirname + "/public"));
 	app.use(express.favicon());
 	app.use(express.bodyParser());
-
-	// app.use(express.cookieParser(config.sessionSecret));  
-	// app.use(express.session({store: new RedisStore()}));
+	app.use(express.cookieParser("shh it's a secret!"));  
+	app.use(express.session({store: new RedisStore()}));
 	app.use(flash());
 });
 //test
@@ -23,44 +25,55 @@ server.listen(3000);
 app.get("/", function(req, res){
 	res.render("index.html");
 });
-var online = [];
-socket.sockets.on("connection", function(s){
-
-	s.on("message", function(message){
-		s.get("username", function(err, username){
-			socket.sockets.emit("message", username, message);
-		})//commentcomment
-		
-	});
-	s.on("username", function(username){
-		var idx = online.push(username) -1;
-		s.set("username", username);
-		socket.sockets.emit("message", username + " joined the room!");
-		s.set("index", idx);
-		socket.sockets.emit("online", online);
-	});
-	s.on("disconnect", function(){
-		s.get("index", function(err, index){
-			s.get("username", function(err, username){
-				online.splice(index, 1);
-				socket.sockets.emit("online", online);
-				socket.sockets.emit("message", username + " left the room!");
+(function(socket, client){
+	var online = [];
+	socket.sockets.on("connection", function(s){
+		client.lrange("messages", 0,50, function(err, list){
+			var li = [];
+			list.forEach(function(l){
+				li.push(JSON.parse(l));
 			})
-		})
-		
-	});
-	s.on("newUsername", function(name){
-		s.get("username", function(err, username){
-			socket.sockets.emit("message", username + " changed their username to "+name);
-			s.set("username", name);
-			s.get("index", function(err, index){
-				online[index] = name;
-				socket.sockets.emit("online", online);
-			});
+			s.emit("messages", li);
 		});
-		
+		//s.emit("messages", )
+		s.on("message", function(message){
+			s.get("username", function(err, username){
+				socket.sockets.emit("message", username, message);
+				client.rpush("messages", JSON.stringify({"username":username, "message":message, time:(+new Date())}))
+			})//commentcomment
+			
+		});
+		s.on("username", function(username){
+			var idx = online.push(username) -1;
+			s.set("username", username);
+			socket.sockets.emit("message", username + " joined the room!");
+			s.set("index", idx);
+			socket.sockets.emit("online", online);
+		});
+		s.on("disconnect", function(){
+			s.get("index", function(err, index){
+				s.get("username", function(err, username){
+					online.splice(index, 1);
+					socket.sockets.emit("online", online);
+					socket.sockets.emit("message", username + " left the room!");
+				})
+			})
+			
+		});
+		s.on("newUsername", function(name){
+			s.get("username", function(err, username){
+				socket.sockets.emit("message", username + " changed their username to "+name);
+				s.set("username", name);
+				s.get("index", function(err, index){
+					online[index] = name;
+					socket.sockets.emit("online", online);
+				});
+			});
+			
+		});
 	});
-});
+}(socket, client))
+
 
 
 
